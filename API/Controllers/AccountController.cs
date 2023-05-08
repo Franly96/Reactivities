18d31bfd9aef22cd0c024d11c3f4bb5abbe8d9f0
Franly Pena <22371923+Franly96@users.noreply.github.com>
2023-05-08 03:37:51 -0400

    Authentication and Autorization added

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+using API.DTOs;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> userManager;
+        public TokenService TokenService { get; }
+        public AccountController(UserManager<AppUser> userManager, TokenService tokenService)
+        {
+            this.TokenService = tokenService;
+            this.userManager = userManager;
+
+        }
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
+        {
+            var user = await this.userManager.FindByEmailAsync(loginDTO.Email);
+            if (user == null) return Unauthorized();
+            var result = await this.userManager.CheckPasswordAsync(user, loginDTO.Password);
+            if (result)
+            {
+                return CreateUserObject(user);
+            }
+            return Unauthorized();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO)
+        {
+            if (await this.userManager.Users.AnyAsync(x => x.UserName == registerDTO.Username))
+            {
+                return BadRequest("username is already taken");
+            }
+
+            if (await this.userManager.Users.AnyAsync(x => x.Email == registerDTO.Email))
+            {
+                return BadRequest("Email is already taken");
+            }
+            var user = new AppUser { DisplayName = registerDTO.DisplayName, Email = registerDTO.Email, UserName = registerDTO.Username };
+            var result = await this.userManager.CreateAsync(user, registerDTO.Password);
+            if (result.Succeeded)
+            {
+                return CreateUserObject(user);
+            }
+            return BadRequest(result.Errors);
+        }
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDTO>> GetCurrentUser()
+        {
+            var user = await this.userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+            return CreateUserObject(user);
+        }
+
+
+        private UserDTO CreateUserObject(AppUser user)
+        {
+            return new UserDTO
+            {
+                DisplayName = user.DisplayName,
+                Image = null,
+                Token = this.TokenService.CreateToken(user),
+                Username = user.UserName
+            };
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,9 @@
 using API.Extensions;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -7,9 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => {
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 builder.Services.AddApplicationServices(builder.Configuration);
-
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,7 +28,9 @@ if (app.Environment.IsDevelopment())
 }
 app.UseCors("CorsPolicy");
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
@@ -31,8 +40,9 @@ var service = scope.ServiceProvider;
 try
 {
     var context = service.GetRequiredService<DataContext>();
+    var userManager = service.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
